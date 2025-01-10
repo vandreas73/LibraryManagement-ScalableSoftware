3fae09a6c7fba29624d5eb9403f185427031410a
vandreas73 <113762114+vandreas73@users.noreply.github.com>
2025-01-10 13:43:44 +0100

    grpc sample project works

---
+++ UserManagementServiceClient/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using UserManagementServiceClient;
+
+Console.WriteLine("Hello, World!");
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7275");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine($"Greeting {reply.Message}");
