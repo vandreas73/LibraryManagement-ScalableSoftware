793155367ca460866cef2668e46673c3954ff95e
vandreas73 <113762114+vandreas73@users.noreply.github.com>
2025-01-11 16:07:19 +0100

    no seeddata

--- UserManagementService/Models/SeedData.cs
+++ UserManagementService/Models/SeedData.cs
@@ -4,23 +4,23 @@
 	{
 		public static void Initialize(IServiceProvider serviceProvider)
 		{
-			var context = serviceProvider.GetRequiredService<UserContext>();
-			if (!context.Users.Any())
-			{
-				context.Users.AddRange(
-					new UserModel
-					{
-						Name = "András",
-						Email = "a@a.com",
-						Address = "1111 Budapest, Műegyetem rkp. 3."
-					},
-					new UserModel
-					{
-						Name = "Béla",
-						Email = "b@b.com",
-						Address = "1234 Alsókarakószörcsög, Kossuth u. 1."
-					});
-				context.SaveChanges();
+			//var context = serviceProvider.GetRequiredService<UserContext>();
+			//if (!context.Users.Any())
+			//{
+			//	context.Users.AddRange(
+			//		new UserModel
+			//		{
+			//			Name = "András",
+			//			Email = "a@a.com",
+			//			Address = "1111 Budapest, Műegyetem rkp. 3."
+			//		},
+			//		new UserModel
+			//		{
+			//			Name = "Béla",
+			//			Email = "b@b.com",
+			//			Address = "1234 Alsókarakószörcsög, Kossuth u. 1."
+			//		});
+			//	context.SaveChanges();
 			}
 		}
 	}
